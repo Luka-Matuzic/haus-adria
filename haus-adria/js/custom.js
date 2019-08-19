e7b6c59aa39632502c4e0a06617f68a04ef774c3
Luka Matuzić <36075560+Luka-Matuzic@users.noreply.github.com>
2019-08-19 19:12:19 +0200

    Add files via upload

---
+++ haus-adria/js/custom.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    
+    'use strict';
+    
+    setInterval(function(){
+        
+        'use strict';
+        
+        var windowHeight=$(window).height();
+        var containerHeight = $(".header-container").height();
+        
+        var padTop= windowHeight-containerHeight;
+        
+        $(".header-container").css({
+            'padding-top': Math.round (padTop/2)+ 'px',
+            'padding-bottom': Math.round(padTop/2) + 'px'    
+        });
+    }, 10);
+});
+
+
+
+
+
+/*HOVER - DROP-DOWN MENU*/
+$('ul.nav li.dropdown').hover(function(){
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+
+
+
+
+
+/*Show and hide menu*/
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 70) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
+
+
+
